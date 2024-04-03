0a52edffb7d01b7f0b7b5948055f385daed77883
Manish Shrestha <manishshtestha472@gmail.com>
2024-04-03 12:53:40 +0545

    "Add 'Select All' checkbox for facility screen and update UI elements"
    This commit adds a new 'Select All' checkbox to the facility screen, updates the UI to display a circular progress indicator when loading data, and wraps the ElevatedButton in a Material widget for consistent styling. Additionally, the code includes some minor formatting changes for improved readability.

--- lib/features/facility/screen/facility_screen.dart
+++ lib/features/facility/screen/facility_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hostel_nepal/constants/global_variables.dart';
 import 'package:hostel_nepal/features/facility/model/facility_model.dart';
 import 'package:hostel_nepal/features/facility/repository/facility_repo.dart';
 import 'package:hostel_nepal/features/rule/rule_screen/rule.dart';
@@ -16,6 +17,7 @@ class _FacilityMainState extends State<FacilityMain> {
   final FacilityService _facilityService = FacilityService();
   late Future<List<FacilityModel>> _futureFacilities;
   late List<bool> _isSelectedList;
+  bool _selectAll = false;
 
   @override
   void initState() {
@@ -34,12 +36,31 @@ class _FacilityMainState extends State<FacilityMain> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Material(
+              elevation: 10,
+              child: CheckboxListTile(
+                // Step 2: Add a CheckboxListTile for "Select All"
+                activeColor: GlobalVariables.mainColor,
+                title: const Text('Select All'),
+                value: _selectAll,
+                tileColor: Color.fromRGBO(255, 255, 255, 1),
+                onChanged: (bool? value) {
+                  setState(() {
+                    _selectAll = value!;
+                    _isSelectedList =
+                        List<bool>.filled(_isSelectedList.length, _selectAll);
+                  });
+                },
+              ),
+            ),
             Expanded(
               child: FutureBuilder<List<FacilityModel>>(
                 future: _futureFacilities,
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CircularProgressIndicator();
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
                   } else if (snapshot.hasError) {
                     return Text('Error: ${snapshot.error}');
                   } else {
@@ -48,36 +69,35 @@ class _FacilityMainState extends State<FacilityMain> {
                           List<bool>.filled(snapshot.data!.length, false);
                     }
                     return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                        FacilityModel facility = snapshot.data![index];
-                        return ListTile(
-                          title: Text(facility.title),
-                          leading: Checkbox(
-                            value: _isSelectedList[index],
-                            onChanged: (bool? value) {
-                              setState(() {
-                                _isSelectedList[index] = value!;
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          FacilityModel facility = snapshot.data![index];
+                          return CheckboxListTile(
+                              activeColor: GlobalVariables.mainColor,
+                              title: Text(facility.title),
+                              value: _isSelectedList[index],
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  _isSelectedList[index] = value!;
+                                });
                               });
-                            },
-                          ),
-                        );
-                      },
-                    );
+                        });
                   }
                 },
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const RuleForm()));
-                },
-                child: const Text('Next'),
+              child: Material(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const RuleForm()));
+                  },
+                  child: const Text('Next'),
+                ),
               ),
             ),
           ],
